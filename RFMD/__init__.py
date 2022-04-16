5221d085699a346ab74e70d2f498e3abe4cd5c19
Harsh Patel <harssh.s.patel@gmail.com>
2022-04-16 00:14:20 -0400

    init

---
+++ RFMD/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
+import warnings
+
+warnings.filterwarnings( "ignore", category=FutureWarning )
+warnings.filterwarnings( "ignore", category=DeprecationWarning )
+
+app = Flask( __name__ )
+
+app.secret_key = 'BlackThunder'
+
+app.config[ 'PERMANENT_SESSION_LIFETIME' ] = timedelta( minutes=30 )
+
+app.config[ 'TESTING' ] = True
+
+app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS' ] = True
+
+app.config[ 'SQLALCHEMY_ECHO' ] = True
+
+app.config[ 'SQLALCHEMY_RECORD_QUERIES' ] = True
+
+app.config[ 'SQLALCHEMY_DATABASE_URI' ] = 'mysql+pymysql://root@localhost:3306/RFMD'
+
+app.config[ 'SQLALCHEMY_MAX_OVERFLOW' ] = 0
+
+app.config[ 'UPLOAD_FOLDER' ] = 'static/Dataset/Videos'
+
+db = SQLAlchemy( app )
+
+print( "in project" )
+
+import RFMD.com.controller.RegistrationController
